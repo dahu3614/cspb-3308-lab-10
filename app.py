41034eacba9619308f7dd90339c533d06ce91a06
dahu3614 <david.hughes@colorado.edu>
2023-08-01 21:42:54 +0000

    Add create, insert, select, drop routes

--- app.py
+++ app.py
@@ -28,3 +28,50 @@ def creating():
     conn.commit()
     conn.close()
     return "Basketball Table Successfully Created"
+
+@app.route('/db_insert')
+def inserting():
+    conn = psycopg2.connect("postgresql://dahu3614:5Z0ZRrCIgEkRyXVoCBapH96xyvfNoLJl@dpg-cj4nf7qcn0vc73eogr2g-a.oregon-postgres.render.com/dahu3614_cspb3308_lab10_db")
+    cur = conn.cursor()
+    cur.execute('''
+        INSERT INTO Basketball (First, Last, City, Name, Number)
+    Values
+        ('Jayson', 'Tatum', 'Boston', 'Celtics', 0),
+        ('Stephen', 'Curry', 'San Francisco', 'Warriors', 30),
+        ('Nikola', 'Jokic', 'Denver', 'Nuggets', 15),
+        ('Kawhi', 'Leonard', 'Los Angeles', 'Clippers', 2);
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Populated"
+
+@app.route('/db_select')
+def selecting():
+    conn = psycopg2.connect("postgresql://dahu3614:5Z0ZRrCIgEkRyXVoCBapH96xyvfNoLJl@dpg-cj4nf7qcn0vc73eogr2g-a.oregon-postgres.render.com/dahu3614_cspb3308_lab10_db")
+    cur = conn.cursor()
+    cur.execute('''
+        SELECT * FROM Basketball;
+        ''')
+    records = cur.fetchall()
+    conn.close()
+    response_string = ""
+    response_string += "<table>"
+    for player in records:
+        response_string += "<tr>"
+        for info in player:
+            response_string += "<td>{}</td>".format(info)
+        response_string += "</tr>"
+    response_string += "</table>"
+    return response_string
+
+@app.route('/db_drop')
+def dropping():
+    conn = psycopg2.connect("postgresql://dahu3614:5Z0ZRrCIgEkRyXVoCBapH96xyvfNoLJl@dpg-cj4nf7qcn0vc73eogr2g-a.oregon-postgres.render.com/dahu3614_cspb3308_lab10_db")
+    cur = conn.cursor()
+    cur.execute('''
+        DROP TABLE Basketball;
+        ''')
+    conn.commit()
+    conn.close()
+    return "Basketball Table Successfully Dropped"
+
